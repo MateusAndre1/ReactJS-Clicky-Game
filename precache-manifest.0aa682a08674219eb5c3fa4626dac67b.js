4434362d5b267611bca070d19d1f9e8fee5944f9
Andre Roque <mateusandre1@yahoo.com>
2019-10-30 21:41:38 -0400

    Updates

---
+++ precache-manifest.0aa682a08674219eb5c3fa4626dac67b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc633f1c5114a5dc5dab08060cc2681",
+    "url": "/ReactJS-Clicky-Game/index.html"
+  },
+  {
+    "revision": "756da3095cfeed5714b0",
+    "url": "/ReactJS-Clicky-Game/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/ReactJS-Clicky-Game/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "756da3095cfeed5714b0",
+    "url": "/ReactJS-Clicky-Game/static/js/main.39143e8b.chunk.js"
+  },
+  {
+    "revision": "52c9175455b614759a8a",
+    "url": "/ReactJS-Clicky-Game/static/js/runtime-main.7f15b849.js"
+  }
+]);
