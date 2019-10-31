8dfd67f3126259eff0a043f8d25aa2d5999317ff
Andre Roque <mateusandre1@yahoo.com>
2019-10-31 00:02:18 -0400

    styled cards more, clicky game is working

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 function Title(props) {
   
-    return <div className="title"> <h1> ReactJS Clicky Game: Dragon Ball Z Sayains!</h1>
-    <h3>Begin by clicking on a Sayain but only click on each once or be defeated, you weakling!</h3>
+    return <div className="title"> <h1> ReactJS Clicky Game: Dragon Ball Z Saiyans!</h1>
+    <h3>Begin by clicking on a Saiyan but only click on each once or be defeated, you weakling!</h3>
     <h4>Current Score: {props.score} | Top Score: {props.topScore}</h4>
     </div>
 }
