dcea462999df5ab86dfc24dd06523c4472a421ac
Andre Roque <mateusandre1@yahoo.com>
2019-10-31 09:54:42 -0400

    added run deploy for gh pages

--- build/precache-manifest.7a9393c0039c19b5b5ac8236897b0cd4.js
+++ build/precache-manifest.7a9393c0039c19b5b5ac8236897b0cd4.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "daf5db2a6f54f051e8ca11ed4ab8cbb9",
+    "revision": "3e929b901c03ed1af495913ca0de7249",
     "url": "/ReactJS-Clicky-Game/index.html"
   },
   {
-    "revision": "15816f53992e2e222647",
-    "url": "/ReactJS-Clicky-Game/static/css/main.8a23940f.chunk.css"
+    "revision": "587c400ce970af2656f3",
+    "url": "/ReactJS-Clicky-Game/static/css/main.d631d4fa.chunk.css"
   },
   {
     "revision": "8ddf23b8ae8e7f5c8847",
     "url": "/ReactJS-Clicky-Game/static/js/2.19c06209.chunk.js"
   },
   {
-    "revision": "15816f53992e2e222647",
+    "revision": "587c400ce970af2656f3",
     "url": "/ReactJS-Clicky-Game/static/js/main.b5f49ec7.chunk.js"
   },
   {
